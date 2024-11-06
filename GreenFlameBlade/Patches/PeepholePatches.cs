ed8baef404127a8a824ecef18a3dd94159e8c3ed
Joshua Thome <joshpthome@gmail.com>
2024-11-06 12:45:39 -0600

    Fix peephole softlock when dream fire floods

---
+++ GreenFlameBlade/Patches/PeepholePatches.cs
@@ -0,0 +1,20 @@
+﻿using HarmonyLib;
+
+namespace GreenFlameBlade.Patches
+{
+    [HarmonyPatch(typeof(Peephole))]
+    public static class PeepholePatches
+    {
+        [HarmonyPostfix, HarmonyPatch(nameof(Peephole.Update))]
+        public static void Update(Peephole __instance)
+        {
+            if (__instance._peeping && !PlayerState.InDreamWorld())
+            {
+                __instance._peeping = false;
+                __instance._exitClosingEyes = true;
+                __instance._exitTransitioningOut = false;
+                __instance._fadeTimer = 0f;
+            }
+        }
+    }
+}
